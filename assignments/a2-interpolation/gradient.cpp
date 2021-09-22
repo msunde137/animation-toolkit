84c6705fa904729af45c8d8e165eeb4d597c94b7
Mac Sunde <msunde@haverford.edu>
2021-09-22 17:06:06 -0400

    finished bezier and gradient sections

--- assignments/a2-interpolation/gradient.cpp
+++ assignments/a2-interpolation/gradient.cpp
@@ -3,15 +3,42 @@
 using namespace glm;
 
 class Gradient : public atkui::Framework {
- public:
-  Gradient() : atkui::Framework(atkui::Orthographic) {
-  }
+public:
+	Gradient() : atkui::Framework(atkui::Orthographic) {
+		ur = { 1, 1, 0 };
+		ul = { 0, 1, 1 };
+		br = { 1, 0, 1 };
+		bl = { 0, 0, 1 };
+		divisions = 50;
+		startPos = { ((float)Framework::width() / (float)divisions), 
+			((float)Framework::height() / (float)divisions), 0 };
+		endPos = vec3(Framework::width(), Framework::height(), 0) - startPos;
+		scale = { startPos.x, startPos.y, 1 };
+	}
 
-  virtual void scene() {
-  }
+	virtual void scene() {
+
+		for (float i = startPos.x; i <= endPos.x; i += scale.x * 2.0f)
+		{
+			for (float j = startPos.y; j <= endPos.y; j += scale.y * 2.0f)
+			{
+				vec3 c1 = bl * (1 - i / endPos.x) + br * (i / endPos.x);
+				vec3 c2 = ul * (1 - i / endPos.x) + ur * (i / endPos.x);
+				vec3 c3 = c1 * (1 - j / endPos.y) + c2 * (i / endPos.y);
+
+				setColor(c3);
+				drawCube({ i, j, 0 }, scale * 2.0f);
+			}
+		}
+	}
+
+	vec3 ur, ul, br, bl;
+	int divisions;
+	vec3 startPos, endPos;
+	vec3 scale;
 };
 
 int main(int argc, char **argv) {
-  Gradient viewer;
-  viewer.run();
+	Gradient viewer;
+	viewer.run();
 }
--- assignments/a2-interpolation/particlecubic.cpp
+++ assignments/a2-interpolation/particlecubic.cpp
@@ -1,22 +1,123 @@
 #include "atkui/framework.h"
 using namespace glm;
 
+
+struct controlPoint
+{
+	vec3 position, color;
+	float radius;
+	bool selected;
+};
+
 class ParticleCubic : public atkui::Framework {
- public:
-  ParticleCubic() : atkui::Framework(atkui::Orthographic) {
-  }
 
-  void setup() {
-  }
+public:
+	ParticleCubic() : atkui::Framework(atkui::Orthographic) {
+		cp1 = { vec3(50, 50, 0), vec3(0,1,1), 10 };
+		cp2 = { vec3(150, 50, 0), vec3(0,1,1), 10 };
+		cp3 = { vec3(50, 150, 0), vec3(0,1,1), 10 };
+		cp4 = { vec3(150, 150, 0), vec3(0,1,1), 10 };
+	}
 
-  void scene() {
-    setColor(vec3(1));
-    drawSphere(vec3(250, 250, 0), 10);
-  }
+	void setup() {
+	}
+
+	void scene() {
+		vec3 mousePos = vec3(mousePosition().x, Framework::height() - mousePosition().y, 0);
+
+		drawControlPoint(cp1, mousePos);
+		drawControlPoint(cp2, mousePos);
+		drawControlPoint(cp3, mousePos);
+		drawControlPoint(cp4, mousePos);
+
+		setColor(vec3(1, 0, 0));
+		drawLine(cp1.position, cp2.position);
+		drawLine(cp3.position, cp4.position);
+
+		if (keyIsDown(GLFW_KEY_1))
+			bez = true;
+		if (keyIsDown(GLFW_KEY_2))
+			bez = false;
+
+		if (bez)
+			bezierAlgorithm(cp1, cp2, cp3, cp4, 50);
+		else
+			casteljauAlgorithm(cp1, cp2, cp3, cp4, 50);
+	}
+
+	void bezierAlgorithm(controlPoint cp1, controlPoint cp2, controlPoint cp3, controlPoint cp4, int steps)
+	{
+		// function p(t)
+		auto p = [cp1, cp2, cp3, cp4](float t) {
+			return pow((1 - t), 3) * cp1.position + 3 * t * pow((1 - t), 2) * cp2.position + 3 * pow(t, 2) * (1 - t) * cp3.position + pow(t, 3) * cp4.position;
+		};
+
+		setColor(vec3(1, 1, 0));
+		float inc = 1 / (float)steps;
+		for (float t = 0; t <= 1 - inc + .001; t += inc)
+		{
+			vec3 pos1 = p(t);
+			vec3 pos2 = p(t + inc);
+			drawLine(pos1, pos2);
+		}
+	}
+
+	void casteljauAlgorithm(controlPoint cp1, controlPoint cp2, controlPoint cp3, controlPoint cp4, int steps)
+	{
+		// function p(t)
+		auto p = [cp1, cp2, cp3, cp4](float t) {
+			vec3 a = cp1.position * (1.0f - t) + cp2.position * t;
+			vec3 b = cp3.position * (1.0f - t) + cp4.position * t;
+			vec3 c = cp2.position * (1.0f - t) + cp3.position * t;
+
+			vec3 a1 = a * (1.0f - t) + c * t;
+			vec3 b1 = c * (1.0f - t) + b * t;
+			
+			return a1 * (1.0f - t) + b1 * t;
+		};
+
+		setColor(vec3(1, 0, 1));
+		float inc = 1 / (float)steps;
+		for (float i = 0; i < 1 - inc + .001; i += inc)
+		{
+			vec3 p1 = p(i);
+			vec3 p2 = p(i + inc);
+			drawLine(p1, p2);
+		}
+	}
+
+	void drawControlPoint(controlPoint& p, vec3 mousePos)
+	{
+		if (mouseIsDown(GLFW_MOUSE_BUTTON_1) && glm::distance(mousePos, p.position) < p.radius && !mouseDown)
+		{
+			mouseDown = true;
+			p.selected = true;
+		}
+		else if (!mouseIsDown(GLFW_MOUSE_BUTTON_1))
+		{
+			mouseDown = false;
+			p.selected = false;
+		}
+		if (mouseDown && p.selected &&
+			mousePos.x < Framework::width() && mousePos.y < Framework::height() &&
+			mousePos.x > 0 && mousePos.y > 0)
+		{
+			p.position = mousePos;
+		}
+
+		setColor(p.color);
+		drawSphere(p.position, p.radius);
+	}
+
+private:
+	controlPoint cp1, cp2, cp3, cp4;
+	bool mouseDown = false;
+	bool bez = false;
+
 };
 
 int main(int argc, char** argv) {
-  ParticleCubic viewer;
-  viewer.run();
+	ParticleCubic viewer;
+	viewer.run();
 }
 
